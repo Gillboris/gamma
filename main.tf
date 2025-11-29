ba1a11d29b955934830b438174e9b54aa74f7f4a
gillboris <messahodikpe@yahoo.com>
2025-11-28 21:25:12 -0500

    com

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
     region = "us-east-2"
 }
 
-resouces "aws_instance" "example" {
+resource "aws_instance" "example" {
     ami = "ami-0f5fcdfbd140e4ab7"
     instance_type = "t2.micro"
 }
